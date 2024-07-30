2c29939c2dd2d3d761056a0ee842229f0a9e4fad
Cole Miller <cole.miller@canonical.com>
2024-07-30 15:35:02 -0500

    fixup request header
    Signed-off-by: Cole Miller <cole.miller@canonical.com>

--- src/raft/request.h
+++ src/raft/request.h
@@ -32,6 +32,12 @@ static struct sm_conf request_states[REQUEST_NR] = {
 
 static inline bool request_invariant(const struct sm *sm, int prev)
 {
+	/* The next line exists because otherwise request_states would
+	 * be flagged as unused in translation units that include this
+	 * header but don't reference the static (i.e. that don't call
+	 * sm_init). We could avoid this by just declaring the static
+	 * in this header and defining it in request.c, but that splits
+	 * the details of the state machine up in an ugly way. */
 	(void)request_states;
 	(void)sm;
 	(void)prev;
@@ -39,8 +45,7 @@ static inline bool request_invariant(const struct sm *sm, int prev)
 }
 
 /* Abstract request type */
-struct request
-{
+struct request {
 	/* Must be kept in sync with RAFT__REQUEST in raft.h */
 	void *data;
 	int type;
